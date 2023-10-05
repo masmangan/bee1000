2b1250713f136e6577f7e264c30c0612a8797756
masmangan <marco.mangan@gmail.com>
2023-10-05 15:53:35 -0300

    #2

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,8 +1,34 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
 
 class DemoApplicationTests {
 
-	
+	@Test
+	public void testAdd() {
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		PrintStream outputStream = new PrintStream(byteArrayOutputStream);
+
+		PrintStream previousOut = System.out;
+
+		System.setOut(outputStream);
+
+		DemoApplication.main(null);
+
+		String actual = byteArrayOutputStream.toString();
+		String expected = "Hello World!\n";
+
+		outputStream.close();
+
+		System.setOut(previousOut);
+
+		assertEquals(expected, actual);
+
+	}
 
 }
